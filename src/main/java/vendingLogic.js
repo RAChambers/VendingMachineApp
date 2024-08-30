fdbf892d6f3d7d34939a24dc3f498f192a0bb256
Robyn Chambers <rchambe9@gmail.com>
2024-08-30 15:02:25 -0400

    Create intial ui for vending machine calculations

---
+++ src/main/java/vendingLogic.js
@@ -0,0 +1,19 @@
+var moneyInserted = 0;
+var change = 0;
+
+const currency_one = 1;
+const currency_five = 5;
+const currency_nickel = 0.05;
+const currency_dime = 0.1;
+const currency_quarter = 0.25;
+
+function getTotal(){
+    var currency_ones = Number(document.getElementById("ones").value)
+    var currency_fives = Number(document.getElementById("fives").value)
+    var currency_nickels = Number(document.getElementById("nickels").value)
+    var currency_dimes = Number(document.getElementById("dimes").value)
+    var currency_quarters = Number(document.getElementById("quarters").value)
+
+}
+
+
